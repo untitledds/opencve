2eb2d364c4cb8a1254f3c89ef77b3b128b4f8afa
Aleksey Dymchenko <untitledds@ya.ru>
2025-01-23 17:34:22 +0300

    change to gt filter

--- web/cves/extended_utils.py
+++ web/cves/extended_utils.py
@@ -14,12 +14,12 @@ def extended_list_filtered_cves(params, user):
     # Фильтрация по дате создания
     created_at = params.get("created_at")
     if created_at:
-        queryset = queryset.filter(created_at__date=created_at)
+        queryset = queryset.filter(created_at__date__gte=created_at)
 
     # Фильтрация по дате обновления
     updated_at = params.get("updated_at")
     if updated_at:
-        queryset = queryset.filter(updated_at__date=updated_at)
+        queryset = queryset.filter(updated_at__date__gte=updated_at)
 
     return queryset
 
